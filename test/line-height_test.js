89432c8f52cf0ca0754e4d2298dcdd9257a4047a
Todd Wolfson <todd@twolfson.com>
2013-07-06 00:33:34 -0700

    Adding a personal sanity test for 50px styled divs with proper height

--- test/line-height_test.js
+++ test/line-height_test.js
@@ -18,7 +18,6 @@ function assertNotEqual(a, b) {
 describe('An unstyled div', function () {
   before(function () {
     var node = document.createElement('div');
-    node.className = 'test-div';
     node.innerHTML = 'abc';
     document.body.appendChild(node);
     this.node = node;
@@ -41,6 +40,40 @@ describe('An unstyled div', function () {
       var heightStr = computedStyle(this.node, 'height'),
           height = parseInt(heightStr, 10);
       assertEqual(this.lineHeight, height);
+    });
+  });
+});
+
+describe('A styled div', function () {
+  before(function () {
+    var node = document.createElement('div');
+    node.innerHTML = 'abc';
+    node.style.cssText = 'line-height: 50px;';
+    document.body.appendChild(node);
+    this.node = node;
+  });
+  after(function () {
+    document.body.removeChild(this.node);
+  });
+
+  describe('processed by line-height', function () {
+    before(function () {
+      this.lineHeight = lineHeight(this.node);
+    });
+
+    it('has a line-height which is a number', function () {
+      assertEqual(typeof this.lineHeight, 'number');
+      assertNotEqual(isNaN(this.lineHeight), true);
+    });
+
+    it('has a height of 50px', function () {
+      var heightStr = computedStyle(this.node, 'height'),
+          height = parseInt(heightStr, 10);
+      assertEqual(height, 50);
+    });
+
+    it('has the styled line-height', function () {
+      assertEqual(this.lineHeight, 50);
     });
   });
 });
