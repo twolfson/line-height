4bcbdc826e5d70f1c07f5dce90ddf796c0d277ca
Todd Wolfson <todd@twolfson.com>
2013-07-05 23:59:31 -0700

    Entry level test suite is working

--- test/line-height_test.js
+++ test/line-height_test.js
@@ -1,11 +1,35 @@
-var line_height = require('../lib/line-height.js');
+// Load in computedStyle
+var lineHeight = require('../lib/line-height.js'),
+    assert = require('assert'),
+    computedStyle = require('computed-style');
 
-describe('line-height', function () {
+// Basic tests
+describe('An unstyled div', function () {
   before(function () {
-
+    var node = document.createElement('div');
+    node.className = 'test-div';
+    node.innerHTML = 'abc';
+    document.body.appendChild(node);
+    this.node = node;
+  });
+  after(function () {
+    document.body.removeChild(this.node);
   });
 
-  it('', function () {
+  describe('processed by line-height', function () {
+    before(function () {
+      this.lineHeight = lineHeight(this.node);
+    });
 
+    it('has a line-height which is a number', function () {
+      assert.strictEqual(typeof this.lineHeight, 'number');
+      assert.notEqual(isNaN(this.lineHeight), true);
+    });
+
+    it('has a line-height equal to its height', function () {
+      var heightStr = computedStyle(this.node, 'height'),
+          height = parseInt(heightStr, 10);
+      assert.strictEqual(height, this.lineHeight);
+    });
   });
 });
